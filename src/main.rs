9fd45292561ae1498b407ae71d7709cd37fc790c
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2025-02-17 23:19:00 +0530

    Create System Constants

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+const MEMORY_SIZE: u16 = 4096;
+const REGISTERS: u8 = 16;
+const STACK_SIZE: u8 = 16;
+const DISPLAY_HEIGHT: u8 = 32;
+const DISPLAY_WIDTH: u8 = 64;
+const KEYPAD_SIZE: u8 = 16;
+
+struct CHIP8
+{
+
+}
+
 fn main() {
     println!("Hello, world!");
 }
