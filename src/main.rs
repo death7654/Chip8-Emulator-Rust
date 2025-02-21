c1ab86e39b1bfb1657cd7d3db6ac5595bd784a0c
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2025-02-21 18:44:50 +0530

    create emulator structure

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
-const MEMORY_SIZE: u16 = 4096;
-const REGISTERS: u8 = 16;
-const STACK_SIZE: u8 = 16;
-const DISPLAY_HEIGHT: u8 = 32;
-const DISPLAY_WIDTH: u8 = 64;
-const KEYPAD_SIZE: u8 = 16;
+const MEMORY_SIZE: usize = 4096;
+const REGISTERS: usize = 16;
+const STACK_SIZE: usize = 16;
+const DISPLAY_HEIGHT: usize = 32;
+const DISPLAY_WIDTH: usize = 64;
+const KEYPAD_SIZE: usize = 16;
 
-struct CHIP8
+pub struct emulator
 {
-
+    pc: u16, 
+    ram:[u8; MEMORY_SIZE],
+    ram_index: u16,
+    //uses boolean as its just black and white pixels
+    display: [bool;DISPLAY_HEIGHT*DISPLAY_WIDTH],
+    stack: [u16; STACK_SIZE],
+    registers: [u8; REGISTERS],
+    keypad: [bool; KEYPAD_SIZE],
+    sound_timer: u8,
+    delay_timer:  u8
 }
 
 fn main() {
